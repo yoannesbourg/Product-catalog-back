f536a2d3f783e19057d1854df3122a9634694606
Yoann <yoannpascalbouix@gmail.com>
2021-09-25 17:12:15 +0200

    refactor mvc

--- api/product/ProductRoutes.js
+++ api/product/ProductRoutes.js
@@ -9,13 +9,26 @@ const productModel = require("../../models/productSchema")
 app.post("/create", async (req, res) => {
     try {
         const { name, description, price, photo, active } = req.body.newProduct;
+        
+        if (!name || !description || !price || !photo || !active) {
+            return res.json({
+                status: 400,
+                message: 'Missing information in the body request'
+            });
+        }
+
         const newProduct = new productModel({
             name, description, price, photo, active
         });
+
         const result = await newProduct.save();
         return res.json(result);
     } catch (error) {
-        console.error(error.message);
+        console.error('error', error.message)
+        return res.json({
+            status: 500,
+            error: 'Error while creating a product'
+        });
     }
 })
 
@@ -23,11 +36,12 @@ app.post("/create", async (req, res) => {
 app.get("/:page/:filter", async (req, res) => {
     try {
         const { page, filter } = req.params;
+
         if (!page || !filter) {
             return res.json({
                 status: 400,
                 message: 'mising params'
-            })
+            });
         };
 
         const limit = 6;
@@ -47,30 +61,11 @@ app.get("/:page/:filter", async (req, res) => {
         const product = await productModel.find().skip(toSkip).limit(limit);
         res.json(product);
     } catch (error) {
-        console.error(error.message)
-    }
-})
-
-//get all products
-app.get("/filter/:active", async (req, res) => {
-    try {
-        if (!req.params) {
-            return res.json({
-                status: 400,
-                message: 'mising params'
-            })
-        }
-        let active;
-
-        if (req.params === 'true') {
-            active = true;
-        } else if (req.params === 'false') {
-            active = false;
-        }
-        const product = await productModel.find({active: {$eq: active}});
-        res.json(product);
-    } catch (error) {
-        console.error(error.message);
+        console.error('error', error.message)
+        return res.json({
+            status: 500,
+            error: 'Error while creating a product'
+        })
     }
 })
 
@@ -78,27 +73,58 @@ app.get("/filter/:active", async (req, res) => {
 app.get("/:id", async (req, res) => {
     try {
         const { id } = req.params;
+
+        if (!id) {
+            return res.json({
+                status: 400,
+                error: 'Missing params'
+            });
+        }
         const product = await productModel.findOne({ _id: id });
 
         res.json(product);
     } catch (error) {
-        console.error(error.message);
+        console.error('error',error.message)
+        return res.json({
+            status: 500,
+            error: 'Error while creating a product'
+        })
     }
 })
 
 //update a product
 app.put("/update/:id", async (req, res) => {
     try {
-        const { id } = req.params
-        const { name, description, price, photo, active } = req.body.newValues
+        const { id } = req.params;
+        const { name, description, price, photo, active } = req.body.newValues;
+
+        if (!id) {
+            return res.json({
+                status: 400,
+                error: 'Missing params'
+            });
+        }
+
+        if (!name || !description || !price || !photo || !active) {
+            return res.json({
+                status: 400,
+                message: 'Missing information in the body request'
+            });
+        }
+
         const updateProduct = await productModel.updateOne({ _id: id }, {
             $set: {
                 name, description, price, photo, active
             }
         });
+
         return res.json(updateProduct);
     } catch (error) {
-        console.error(error.message);
+        console.error('error',error.message)
+        return res.json({
+            status: 500,
+            error: 'Error while creating a product'
+        })
     }
 })
 
@@ -109,7 +135,11 @@ app.delete("/delete/:id", async (req, res) => {
         const deleteProduct = await productModel.deleteOne({ _id: id });
         res.json(deleteProduct);
     } catch (error) {
-        console.error(error.message);
+        console.error('error',error.message)
+        return res.json({
+            status: 500,
+            error: 'Error while creating a product'
+        })
     }
 })
 
