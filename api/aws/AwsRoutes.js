80e66af8e78fb4ddde971f38af044f6a41f95061
Yoann <yoannpascalbouix@gmail.com>
2021-10-14 15:18:07 +0200

    Upload image to aws

---
+++ api/aws/AwsRoutes.js
@@ -0,0 +1,42 @@
+
+// Required modules
+const express = require("express");
+const app = express();
+
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
+
+const multer = require('multer');
+const { uploadFile } = require("../../service/AwsService");
+const upload = multer({dest: 'uploads/'});
+
+const { uploadFIle, getFileStream } = require('../../service/AwsService');
+
+app.get('/:key', (req, res) => {
+    const key = req.params.key;
+    const readStream = getFileStream(key);
+    readStream.pipe(res);
+})
+
+app.post("/", upload.single('image'), async (req, res) => {
+    try {
+        const file = req.file;
+        console.log(file);
+        const result = await uploadFile(file);
+        await unlinkFile(file.path);
+        console.log(result);
+        const description = req.body.description;
+        return res.json({
+            imagePath: `/images/${result.Key}`
+        });
+    } catch (error) {
+        console.error('error', error.message)
+        return res.json({
+            status: 500,
+            error: 'Error aws'
+        });
+    }
+})
+
+module.exports = app
--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const productsApi = require("./product/ProductRoutes");
+const awsApi = require("./aws/AwsRoutes");
 
 router.use("/products", productsApi);
+router.use("/aws", awsApi);
 
 module.exports = router;
