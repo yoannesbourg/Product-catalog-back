3c39bb4c9cfedf8a48c8e269c6b6cf5bae66a347
Yoann <yoannpascalbouix@gmail.com>
2021-10-25 19:04:18 +0200

    readme aws information +aws service small fixes

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Getting Started
 
+To run this project you need a .env file with those variables:
+
+DATABASE_URL
+PORT
+AWS_BUCKET_NAME
+AWS_BUCKET_REGION
+AWS_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY
+
 - Clone this project (Back)
 - npm install
 - npm run server
-- Clone [front](https://github.com/yoannesbourg/Product-catalog-back/)
+- Clone [front](https://github.com/yoannesbourg/Product-catalog-front)
 - npm install
 - npm run start
--- api/aws/AwsRoutes.js
+++ api/aws/AwsRoutes.js
@@ -8,10 +8,9 @@ const util = require('util');
 const unlinkFile = util.promisify(fs.unlink);
 
 const multer = require('multer');
-const { uploadFile } = require("../../service/AwsService");
 const upload = multer({dest: 'uploads/'});
 
-const { uploadFIle, getFileStream } = require('../../service/AwsService');
+const { uploadFile, getFileStream } = require('../../service/AwsService');
 
 app.get('/:key', (req, res) => {
     const key = req.params.key;
@@ -24,13 +23,12 @@ app.post("/", upload.single('image'), async (req, res) => {
         const file = req.file;
         const result = await uploadFile(file);
         await unlinkFile(file.path);
-        console.log(result);
         return res.json(result);
     } catch (error) {
         console.error('error', error.message)
         return res.json({
             status: 500,
-            error: 'Error aws'
+            error: 'Error when uploading img to aws'
         });
     }
 })
